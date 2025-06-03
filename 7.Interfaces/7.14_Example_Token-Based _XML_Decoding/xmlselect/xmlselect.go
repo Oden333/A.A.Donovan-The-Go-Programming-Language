f2d0e4c6b95ad6a8c1259f470b36c1fbf4c6342e
oden333 <galimov-ismail@mail.ru>
2025-06-03 14:38:41 +0300

    8

--- 7.Interfaces/7.14_Example_Token-Based _XML_Decoding/xmlselect/xmlselect.go
+++ 7.Interfaces/7.14_Example_Token-Based _XML_Decoding/xmlselect/xmlselect.go
@@ -2,21 +2,32 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io"
 	"os"
+	"slices"
 	"strings"
 )
 
 // The xmlselect program extracts and prints the text found beneath certain
 // elements in an XML document tree. Using the API above, it can do its job in a single
 // pass over the input without ever materializing the  tree
-func main() {
-	dec := xml.NewDecoder(os.Stdin)
-	var stack []string // stack of element names
+func ex7_17() {
+	file, err := os.OpenFile("test", 2, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	dec := xml.NewDecoder(file)
+
+	var stack []string        // stack of element names
+	var els = []string{"div"} // = os.Args[1:]
+
+	var tok xml.Token
 	for {
-		tok, err := dec.Token()
+		tok, err = dec.Token()
 		if err == io.EOF {
 			break
 		} else if err != nil {
@@ -26,12 +37,16 @@ func main() {
 		switch tok := tok.(type) {
 		case xml.StartElement:
 			stack = append(stack, tok.Name.Local) // push onto a stack
+			if m := attrsMapping(tok.Attr, els); len(m) > 0 {
+				fmt.Printf("%s: %s\n----------------------\n", strings.Join(stack, " "), m)
+			}
 		case xml.EndElement:
 			stack = stack[:len(stack)-1] // pop the name from the stack.
 		case xml.CharData:
-			if containsAll(stack, os.Args[1:]) {
-				fmt.Printf("%s: %s\n", strings.Join(stack, " "), tok)
+			if containsAll(stack, els) {
+				fmt.Printf("%s: %s\n----------------------\n", strings.Join(stack, " "), tok)
 			}
+
 		}
 	}
 }
@@ -51,4 +66,91 @@ func containsAll(x, y []string) bool {
 		x = x[1:]
 	}
 	return false
+}
+
+func attrsMapping(attrs []xml.Attr, els []string) []string {
+	res := make([]string, 0)
+	for _, a := range attrs {
+		if slices.Contains(els, a.Name.Local) {
+			res = append(res, a.Value)
+		}
+	}
+	return res
+}
+
+func parse(r io.Reader) (Node, error) {
+	dec := xml.NewDecoder(r)
+	// fmt.Println(io.ReadAll(r))
+	var stack []*Element
+	var root Node
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			el := &Element{tok.Name, tok.Attr, nil}
+			if len(stack) == 0 {
+				root = el
+			} else {
+				parent := stack[len(stack)-1]
+				parent.Children = append(parent.Children, el)
+			}
+			stack = append(stack, el) // push
+		case xml.EndElement:
+			stack = stack[:len(stack)-1] // pop
+		case xml.CharData:
+			if len(bytes.TrimSpace(tok)) == 0 {
+				continue
+			}
+			parent := stack[len(stack)-1]
+			parent.Children = append(parent.Children, CharData(tok))
+		}
+	}
+	return root, nil
+}
+
+type Node interface{} // CharData or *Element
+type CharData string
+type Element struct {
+	Type     xml.Name
+	Attr     []xml.Attr
+	Children []Node
+}
+
+func (n *Element) String() string {
+	b := &bytes.Buffer{}
+	visit(n, b, 0)
+	return b.String()
+}
+
+func visit(n Node, w io.Writer, depth int) {
+	switch n := n.(type) {
+	case *Element:
+		fmt.Fprintf(w, "%*s%s %s\n", depth*2, "", n.Type.Local, n.Attr)
+		for _, c := range n.Children {
+			visit(c, w, depth+1)
+		}
+	case CharData:
+		fmt.Fprintf(w, "%*s%q\n", depth*2, "", n)
+	default:
+		panic(fmt.Sprintf("got %T", n))
+	}
+}
+
+func main() {
+	file, err := os.OpenFile("test", 2, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	node, err := parse(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(node)
 }
---
+++ 8.Goroutines_and_Channels/8.1_Goroutines/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Go enables two styles of concurrent programming.
+// This chapter presents goroutines and channels, which support communicating  sequential  processes or CSP,
+// a model of  concurrency  in  which  values  are  passed  between  independent  activities
+// (goroutines) but variables are for the most part confined to a single activity
+
+//! When a program starts, its only goroutine is the one that calls the main function, so we  call  it  the  main  goroutine.
+
+// ! The go statement itself completes immediately
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
