d949867a472ee9c6a4ee8aa3158e81225d345568
oden333 <galimov-ismail@mail.ru>
2025-06-23 17:56:24 +0300

    1123

---
+++ 11.Testing/11.2_Test_Functions/11.2.3_White-Box_Testing/main.go
@@ -0,0 +1,17 @@
+package whiteboxtesting
+
+//? One way of categorizing tests is by the level of knowledge they require of the internal
+//? workings of the package under test
+
+//* A black-box test assumes nothing about the package other than what is exposed
+//* by its API and specified by its documentation; the package’s internals are opaque.
+
+//* In contrast, a white-box test has privileged access to the internal functions and data structures
+//* of the package and can make observations and changes that an ordinary client cannot.
+
+// For example, a white-box test can check that the invariants of the package’s data types
+// are maintained after every operation.
+//
+// TestIsPalindrome calls only the exported function IsPalindrome and is thus a black-box test.
+// TestEcho  calls the  echo  function  and  updates  the  global  variable  out,  both  of  which  are
+// unexported, making it a white-box test
--- 11.Testing/11.2_Test_Functions/main.go
+++ 11.Testing/11.2_Test_Functions/main.go
@@ -1,6 +1,13 @@
 package testfunctions
 
-import "testing"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
 
 // ! Each test file must import the testing package.
 // ! Test functions have the following signature
@@ -8,5 +15,36 @@ func TestName(t *testing.T) {
 	// ...
 }
 
-// ? The t parameter provides methods for reporting test failures and
-// ? logging additional information.
+//* The t parameter provides methods for reporting test failures and
+//* logging additional information.
+
+//? A package named main ordinarily produces an executable program,
+//? but it can be imported as a library too
+
+var (
+	n = flag.Bool("n", false, "omit trailing newline")
+	s = flag.String("s", " ", "separator")
+)
+
+var out io.Writer = os.Stdout // modified during testing
+
+func main() {
+	flag.Parse()
+	if err := echo(!*n, *s, flag.Args()); err != nil {
+		fmt.Fprintf(os.Stderr, "echo: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func echo(newline bool, sep string, args []string) error {
+	fmt.Fprint(out, strings.Join(args, sep))
+	if newline {
+		fmt.Fprintln(out)
+	}
+	return nil
+}
+
+//? Notice that the test code is in the same package as the production code. Although the
+//? package name is main and it defines a main function, during testing this package
+//? acts as a library that exposes the function TestEcho to the test driver;
+//? its main function is ignored.
