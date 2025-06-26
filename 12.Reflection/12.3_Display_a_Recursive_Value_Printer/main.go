8e4f2c58a23d59c7acc36c83e5a7fc2a8d6413e1
oden333 <galimov-ismail@mail.ru>
2025-06-26 12:02:39 +0300

    tst

--- 12.Reflection/12.3_Display_a_Recursive_Value_Printer/main.go
+++ 12.Reflection/12.3_Display_a_Recursive_Value_Printer/main.go
@@ -1,1 +1,8 @@
 package main
+
+import "github.com/Oden333/A.A.Donovan-The-Go-Programming-Language/tree/master/7.Interfaces/7.9_Example_Expression_Evaluator/eval"
+
+func main() {
+	e, _ := eval.Parse("sqrt(A / pi)")
+	eval.Display("e", e)
+}
